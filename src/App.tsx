68bca87a41a46bfa3ee4058ccd7be741a2c467cb
NNQA <anhla12h@gmail.com>
2024-01-24 23:03:16 +0700

    add: add router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="bg-black"></div>;
+  return <Routes></Routes>;
 }
 
 export default App;
